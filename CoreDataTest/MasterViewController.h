62f9a6c4bb9552417ebdfa33ea9ac3d87fc754d2
jmcoyne <jcoyne@mzinga.com>
2014-09-30 12:56:12 -0400

    Initial Commit

---
+++ CoreDataTest/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  CoreDataTest
+//
+//  Created by Joan Coyne on 9/30/14.
+//  Copyright (c) 2014 Mzinga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
